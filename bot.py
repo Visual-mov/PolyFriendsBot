1e0004cc1b3a0625a3c3c846ee80016406990b42
Ryan D <ryandanver@gmail.com>
2020-06-24 16:38:00 -0700

    Changed file structure and how files are opened

--- bot.py
+++ bot.py
@@ -9,12 +9,12 @@ from image_generator import PolyFriendGenerator
 # Created by Ryan Danver 2020
 
 # Files
-KEYS_PATH = "./keys.txt"
+KEYS_PATH = "keys.txt"
 
-NAMES = "./text/names.txt"
-HOBBIES = "./text/hobbies.txt"
-COLORS = "./text/colors.txt"
-FONT = "./PixelSplitter-Bold.ttf"
+NAMES = "text/names.txt"
+HOBBIES = "text/hobbies.txt"
+COLORS = "text/colors.txt"
+FONT = "PixelSplitter-Bold.ttf"
 
 SEED = ""
 
@@ -30,7 +30,7 @@ def main(argv):
     time = datetime.now()
     file = get_filename(time) if save_date else "img.png"
 
-    generator = PolyFriendGenerator((1000,1000), 5, rand_text(NAMES), FONT, file, SEED)
+    generator = PolyFriendGenerator((1000,1000), 5, rand_text(NAMES), real_path(FONT), real_path(file), SEED)
     generator.generate_image()
     generator.save_image()
 
@@ -52,24 +52,27 @@ def generate_status(name, time):
     minutes = time.minute if len(str(time.minute)) != 1 else "0" + str(time.minute)
     return f"This is {name}, and they like {hobby}!\nTheir favorite color is {color}.\nCreated on {time.month}/{time.day}/{time.year} at {time.hour}:{minutes} {tzname[0]}"
 
-def get_filename(time):
-    return f"img_{time.month}{time.day}{time.year}{time.hour}{time.minute}{time.second}.png"
-
 def rand_text(file):
     try:
-        return choice([s.replace('\n','').lower() for s in open(file,"r").readlines()])
+        return choice([s.replace('\n','').lower() for s in open(real_path(file),"r").readlines()])
     except FileNotFoundError:
         print(f"Could not find {file}")
         exit()
 
 def getkeys():
     try:
-        lines = open(KEYS_PATH,'r').readlines()
+        lines = open(real_path(KEYS_PATH),'r').readlines()
     except FileNotFoundError:
         print("keys.txt not found")
         exit()
     for i in range(len(lines)):
         if i < 4: yield lines[i].replace('\n','')
+
+def real_path(file):
+    return f"{os.path.dirname(os.path.realpath(__file__))}/{file}"
+
+def get_filename(time):
+    return f"img_{time.month}{time.day}{time.year}{time.hour}{time.minute}{time.second}.png"
 
 if __name__ == "__main__":
     main(sys.argv)
